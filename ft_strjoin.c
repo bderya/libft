93a69f2e8909dc1c04c85e8dcc3423f099f27453
bderya <94176553+bderya@users.noreply.github.com>
2022-04-08 19:06:15 +0200

    FILEEE

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,26 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bderya <bderya@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bderya <bderya@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/29 17:06:46 by bderya            #+#    #+#             */
-/*   Updated: 2022/03/29 17:06:46 by bderya           ###   ########.fr       */
+/*   Created: 2022/04/08 09:27:04 by bderya            #+#    #+#             */
+/*   Updated: 2022/04/08 09:27:04 by bderya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+static char	*ft_destination(char const *s1, char const *s2, char *ret)
 {
-	char	*ret;
 	size_t	src_idx;
 	size_t	dst_idx;
 
-	if (s1 && s2)
-	{
-	ret = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!ret)
-		return (NULL);
 	src_idx = 0;
 	dst_idx = 0;
 	while (s1[src_idx])
@@ -38,6 +32,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	}
 	ret[dst_idx] = 0;
 	return (ret);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ret;
+
+	if (s1 && s2)
+	{
+		ret = (char *)malloc
+			(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+		if (!ret)
+			return (NULL);
+		ft_destination(s1, s2, ret);
+		return (ret);
 	}
 	return (NULL);
 }
