43c8541221df7cdb6a6f1da05ec87422145f4bac
bderya <94176553+bderya@users.noreply.github.com>
2022-04-11 02:23:20 +0200

    Add files via upload

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,13 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bderya <bderya@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bderya <bderya@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/29 12:35:28 by bderya            #+#    #+#             */
-/*   Updated: 2022/03/29 15:54:59 by bderya           ###   ########.fr       */
+/*   Created: 2022/04/11 02:05:38 by bderya            #+#    #+#             */
+/*   Updated: 2022/04/11 02:05:38 by bderya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* In this area we should protect about SIZE_MAX and SIZE_MIN --> <limits.h> MACRO IS LIFE
+Like if(SIZE_MAX >= count || SIZE_MIN <= count)
+			return(NULL);
+	 if(SIZE_MAX >= size || SIZE_MIN <= size)
+	 		return(NULL);
+Because SIZE_MAX --> 65535;
+		SIZE_MIN --> 
+*/
 #include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
